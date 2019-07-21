bb09f5e40d829724414af6045ee6680d49b7b069
panevin <panevin88@gmail.com>
2019-07-21 16:24:27 +0300

    закинул что есть

---
+++ Assets/ScoreInspector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreInspector", menuName = "ScoreInspector", order = 1)]
+public class ScoreInspector : ScriptableObject
+{
+    static int score = 0;
+
+    public static void addScore()
+    {
+        score = score+1;
+        Debug.Log(score);
+    }
+
+    public static void resetScore()
+    {
+        score = 0;
+    }
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Supercyan Character Pack Zombie Sample/BasicZombie.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BasicZombie : MonoBehaviour
+{
+    Animator anim;
+
+    public float hp = 100;
+    NavMeshAgent agent;  
+
+
+
+    public void Damage(float dmg)
+    {
+        hp = hp - dmg;
+    }
+
+    void Death()
+    {
+        CapsuleCollider coll = transform.GetComponent<CapsuleCollider>();
+        coll.enabled = false;
+        agent.updatePosition = false;
+        agent.angularSpeed = 0;
+        agent.speed = 0;
+        
+        anim.SetTrigger("death");
+        //anim.ResetTrigger("death");
+        Invoke("destroy", 2);
+    }
+    public void Destination(Vector3 dest)
+    {
+        
+        agent.SetDestination(dest);
+    }
+
+    void destroy()
+    {
+        Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = transform.GetComponent<NavMeshAgent>();
+        anim = transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Death();
+        }
+       
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "dest")
+        {
+            Death();
+        }
+    }
+
+}
